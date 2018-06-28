579810412e1abb9730bbdb7a1aa0c8c91e5a3ac9
Michael Davis <mcd164@miami.edu>
2018-06-27 22:41:36 -0500

    Add a cast method for getting a particular type

--- lib/mole/content/scrape.ex
+++ lib/mole/content/scrape.ex
@@ -8,12 +8,14 @@ defmodule Mole.Content.Scrape do
   alias Mole.Content.Meta
 
   @db_module Mole.Content.Isic
-  @amount 20
+  @std_chunk_size 20
   @max_amount Application.get_env(:mole, :max_amount)
   # 1 seconds
   @time_buffer 1_000
   # 30 seconds
   @await_time 30 * 1_000
+  # range of malignant images, in percentage
+  @malignant_range 35..40
 
   @doc "Start the scraper as a worker"
   @spec start_link(any()) :: GenServer.on_start()
@@ -49,10 +51,10 @@ defmodule Mole.Content.Scrape do
   def handle_info(:chunk, offset) do
     Logger.info("Received request to get chunk at offset #{offset}")
 
-    if @amount + offset <= @max_amount do
+    if @std_chunk_size + offset <= @max_amount do
       Logger.info("Gettting a new chunk, at offset #{offset}")
 
-      download(@amount, offset)
+      download(@std_chunk_size, offset)
 
       Process.send_after(self(), :chunk, @time_buffer)
     else
@@ -62,8 +64,22 @@ defmodule Mole.Content.Scrape do
     {:noreply, Mole.Content.count_images()}
   end
 
+  @doc """
+  Onboard a set of images that are only either malignant or benign.
+
+  The `type` parameter is `true` for getting malignant images, false for
+  getting `benign images.
+  """
+  @impl true
+  @spec handle_cast({:chunk, boolean(), integer()}, integer()) :: {:noreply, integer()}
+  def handle_cast({:chunk, _type, _amount}, _offset) do
+    # TODO
+
+    {:noreply, Mole.Content.count_images()}
+  end
+
   private do
-    @doc "Download a chunk from a Source and save it to disk and database"
+    # Download a chunk from a Source and save it to disk and database
     @spec download(integer(), integer()) :: any()
     def download(amount, offset) do
       amount
@@ -71,7 +87,7 @@ defmodule Mole.Content.Scrape do
       |> save_all()
     end
 
-    @doc "Map a function to each element of a collection in parallel."
+    # Map a function to each element of a collection in parallel.
     @spec pmap(Enum.t(), function()) :: Enum.t()
     def pmap(enumerable, fun) do
       enumerable
@@ -79,7 +95,7 @@ defmodule Mole.Content.Scrape do
       |> Enum.map(&Task.await(&1, @await_time))
     end
 
-    @doc "Save all meta structs in a list to local datastore and database."
+    # Save all meta structs in a list to local datastore and database.
     @spec save_all({:ok, list(Meta.t())}) :: Enum.t()
     def save_all({:ok, data}) do
       data
@@ -100,7 +116,7 @@ defmodule Mole.Content.Scrape do
       :ok
     end
 
-    @doc "Save a single meta structure to the local datastore and database."
+    # Save a single meta structure to the local datastore and database.
     @spec save(Meta.t()) :: :ok
     def save(%Meta{id: id} = meta) do
       id
@@ -111,7 +127,7 @@ defmodule Mole.Content.Scrape do
       meta
     end
 
-    @doc "Write a raw image binary to a file"
+    # Write a raw image binary to a file
     @spec write(File.t(), {:ok, binary()}) :: :ok | {:error, term()}
     def write(file, {:ok, data}), do: IO.binwrite(file, data)
 
@@ -126,42 +142,60 @@ defmodule Mole.Content.Scrape do
       :ok
     end
 
-    @doc "Insert a Meta struct into the Image Ecto database"
+    # Insert a Meta struct into the Image Ecto database
     @spec insert(Meta.t()) :: {:ok, Ecto.Schema.t()} | {:error, Ecto.Changeset.t()}
     def insert(%Meta{id: id, malignant?: mal}) do
       %{origin_id: id, malignant: mal, path: static_path(id)}
       |> Mole.Content.create_image()
     end
 
-    @doc "Produce a static path in which to save the image"
+    # Produce a static path in which to save the image
     @spec static_path(String.t()) :: String.t()
     def static_path(id), do: "./priv/static/images/#{id}.jpeg"
 
     import Ecto.Query
 
-    # 30%
-    # @ratio_malignant_percent 0.3
-
     # Ecto query to select malignant images
     @malignant_query from(i in "images", where: [malignant: "TRUE"], select: i.id)
 
-    @doc "Get the ratio of malignant images in the local datastore."
-    @spec ratio_malignant(integer()) :: float()
-    def ratio_malignant(total_amount) do
-      Mole.Repo.aggregate(@malignant_query, :count, :id) / total_amount
+    # Get the percent of malignant images in the local datastore.
+    @spec percent_malignant(integer()) :: float()
+    def percent_malignant(total_amount) do
+      @malignant_query
+      |> Mole.Repo.aggregate(:count, :id)
+      |> Kernel./(total_amount)
+      |> Kernel.*(100)
+      |> round()
     end
 
-    @doc "Determine the percentage of malignant images in the Repo."
+    # Determine the percentage of malignant images in the Repo
     @spec examine_statistics(integer()) :: :ok
     def examine_statistics(amount) do
       Logger.info("Done. Examining statistics.")
 
-      percent = ratio_malignant(amount)
+      percent = percent_malignant(amount)
 
       Logger.info("There are #{amount} images total")
-      Logger.info("#{round(percent * 100)}% of which are malignant.")
+      Logger.info("#{percent}% of which are malignant.")
+
+      enforce_ratio(percent, amount)
 
       :ok
     end
+
+    def enforce_ratio(percent, _total) when percent in @malignant_range, do: :ok
+
+    def enforce_ratio(_percent, total) do
+      m = midpoint(@malignant_range)
+
+      # TODO: check this math for negative condition
+      amount_outstanding = round(m * total / (100 - m))
+
+      type = amount_outstanding < 0
+
+      GenServer.cast(__MODULE__, {:chunk, type, abs(amount_outstanding)})
+    end
+
+    defp midpoint(a..b), do: round((a + b) / 2)
   end
 end
