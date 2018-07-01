21747118e1f8448a5613cae9f14977a765a387c0
Michael Davis <mcd164@miami.edu>
2018-07-01 14:36:34 -0500

    Setup authentication for the socket
    Unsuccessfully so far, though

--- lib/mole_web/router.ex
+++ lib/mole_web/router.ex
@@ -8,6 +8,7 @@ defmodule MoleWeb.Router do
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
     plug(MoleWeb.Auth)
+    plug(:put_user_token)
   end
 
   pipeline :api do
@@ -21,5 +22,15 @@ defmodule MoleWeb.Router do
     get("/play", GameController, :index)
     resources("/users", UserController, only: [:show, :new, :create, :index])
     resources("/sessions", SessionController, only: [:new, :create, :delete])
+  end
+
+  defp put_user_token(conn, _) do
+    if current_user = conn.assigns[:current_user] do
+      token = Phoenix.Token.sign(conn, "user socket", current_user.id)
+
+      assign(conn, :user_token, token)
+    else
+      conn
+    end
   end
 end
