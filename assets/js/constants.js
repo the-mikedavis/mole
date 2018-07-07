492ddb93f1c37818dab526d46efd5ec63057482a
Michael Davis <mcd164@miami.edu>
2018-07-07 16:25:10 -0500

    Add small amount of feedback to question answer

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,7 +1,17 @@
+const FADE_TIME = 3000  // 3 seconds
+
 const constants = {
   tinder_event_name: 'trigger',
   image_event_name: 'new_image',
-  correct_event_name: 'answer_reply'
+  correct_event_name: 'answer_reply',
+  activate_element: function (element) {
+    element.classList.add('active')
+    fade(element)
+  }
+}
+
+function fade(element) {
+  setTimeout(() => element.classList.remove('active'), FADE_TIME)
 }
 
 export default constants
--- assets/js/swipe_highlighter.js
+++ assets/js/swipe_highlighter.js
@@ -1,23 +1,13 @@
 /* Handle a swipe event from `tinder.js` */
 import image_listener from './image_listener'
-
-const FADE_TIME = 3000  // 3 seconds
-const type_tell = document.getElementById('type-tell')
-const benign_el = type_tell.firstChild
-const malignant_el = type_tell.lastChild
+import constants from './constants'
 
 function listener(event) {
+  const type_tell = document.getElementById('type-tell')
+  const benign_el = type_tell.firstChild
+  const malignant_el = document.querySelector('span.right')
   // malignant = true, benign = false
-  activate(event.detail ? malignant_el : benign_el)
-}
-
-function activate(element) {
-  element.classList.add('active')
-  fade(element)
-}
-
-function fade(element) {
-  setTimeout(() => element.classList.remove('active'), FADE_TIME)
+  constants.activate_element(event.detail ? malignant_el : benign_el)
 }
 
 export default listener
