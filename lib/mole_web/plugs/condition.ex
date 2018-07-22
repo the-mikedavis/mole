f2efafe053ddf205ef58d252b5f119dc4649662a
Michael Davis <mcd164@miami.edu>
2018-07-21 20:18:29 -0700

    Create a lazy plug for conditions

---
+++ lib/mole_web/plugs/condition.ex
@@ -0,0 +1,31 @@
+defmodule MoleWeb.Plugs.Condition do
+  @moduledoc false
+  import Plug.Conn
+
+  alias Mole.{Accounts, Accounts.User}
+
+  # dry
+  @key :condition
+
+  @eds [:abcde, :duckling, :none]
+  @feedback [true, false]
+  # the cartesian product of the above
+  @conditions for ed <- @eds, fb <- @feedback, do: {ed, fb}
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    condition = get_session(conn, @key)
+
+    with %User{condition: nil} = user <- conn.assigns.current_user,
+         do: Accounts.update_user(user, %{condition: condition})
+
+    assign(conn, @key, condition)
+  end
+
+  # give a random condition to a user
+  def put_random(conn), do: put_session(conn, @key, Enum.random(0..5))
+
+  # get back the useful information from the conditions
+  def to_tuple(index), do: Enum.at(@conditions, index)
+end
