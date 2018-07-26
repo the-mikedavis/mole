cebd5e2905f9e8fd882a57b47e221beb259f0713
Michael Davis <mcd164@miami.edu>
2018-07-25 16:40:58 -1000

    Get most of isic, just missing bad json

--- test/mole/content/isic_test.exs
+++ test/mole/content/isic_test.exs
@@ -65,4 +65,72 @@ defmodule Mole.Content.IsicTest do
   test "http_client/1 is producing the moxed version in :test" do
     assert Isic.http_client() == HTTPoisonMock
   end
+
+  describe "getting a chunk, all the way through," do
+    setup do
+      rand_5_json = File.read!("test/support/5-random.json")
+      rand_5_http = {:ok, %HTTPoison.Response{body: rand_5_json}}
+
+      rand_5_metas =
+        {:ok,
+         [
+           %Mole.Content.Meta{
+             id: "5436e3abbae478396759f0cf",
+             malignant?: false
+           },
+           %Mole.Content.Meta{
+             id: "5436e3acbae478396759f0d1",
+             malignant?: false
+           },
+           %Mole.Content.Meta{
+             id: "5436e3acbae478396759f0d3",
+             malignant?: true
+           },
+           %Mole.Content.Meta{
+             id: "5436e3acbae478396759f0d5",
+             malignant?: false
+           },
+           %Mole.Content.Meta{
+             id: "5436e3acbae478396759f0d7",
+             malignant?: true
+           }
+         ]}
+
+      mal_5_metas =
+        {:ok,
+         [
+           %Mole.Content.Meta{
+             id: "5436e3acbae478396759f0d3",
+             malignant?: true
+           },
+           %Mole.Content.Meta{
+             id: "5436e3acbae478396759f0d7",
+             malignant?: true
+           }
+         ]}
+
+      [
+        rand_5_http: rand_5_http,
+        rand_5_metas: rand_5_metas,
+        mal_5_metas: mal_5_metas
+      ]
+    end
+
+    test "of size 5", c do
+      Mox.expect(HTTPoisonMock, :get, fn url ->
+        assert url ==
+                 "https://isic-archive.com/api/v1/image?limit=5&offset=0&sort=_id&sortdir=1&detail=true"
+
+        c.rand_5_http
+      end)
+
+      assert Isic.get_chunk(5, 0) == c.rand_5_metas
+    end
+
+    test "of size 5, malignant", c do
+      Mox.expect(HTTPoisonMock, :get, fn _url -> c.rand_5_http end)
+
+      assert Isic.get_chunk(5, 0, malignant?: true) == c.mal_5_metas
+    end
+  end
 end
