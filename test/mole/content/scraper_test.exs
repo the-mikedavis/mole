0c46a8c97239153f15cdcf62bbcd6ea24c4502d1
Michael Davis <mcd164@miami.edu>
2018-07-27 21:03:42 -1000

    Add capture log stuff

--- test/mole/content/scraper_test.exs
+++ test/mole/content/scraper_test.exs
@@ -2,6 +2,7 @@ defmodule Mole.ScrapeTest do
   use ExUnit.Case
   # use Mole.DataCase
   alias Mole.Content.Scrape
+  import ExUnit.CaptureLog
 
   @malignant_range 28..32
 
@@ -36,11 +37,15 @@ defmodule Mole.ScrapeTest do
 
   describe "All error-based functions return `:ok`" do
     test "the save_all/1 function" do
-      assert Scrape.save_all({:error, "reason"}) == :ok
+      assert capture_log(fn ->
+        assert Scrape.save_all({:error, "reason"}) == :ok
+      end) =~ ~S([warn] Error occurred saving... Reason: "reason"")
     end
 
     test "the write/2 function" do
-      assert Scrape.write(nil, {:error, "reason"}) == :ok
+      assert capture_log(fn ->
+        assert Scrape.write(nil, {:error, "reason"}) == :ok
+      end) =~ ~S([warn] Error occurred saving... Reason: "reason"")
     end
   end
 end
