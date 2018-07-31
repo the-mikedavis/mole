58fccf9cc6e096e0cebca45ab0623bc5e40ec35b
Michael Davis <mcd164@miami.edu>
2018-07-30 21:28:27 -0500

    Access with the square braces

--- lib/mole_web/plugs/feedback.ex
+++ lib/mole_web/plugs/feedback.ex
@@ -7,8 +7,8 @@ defmodule MoleWeb.Plugs.Feedback do
 
   def call(conn, _opts) do
     feedback? =
-      case conn.assigns do
-        %{current_user: %User{condition: con}} -> Condition.feedback?(con)
+      case conn.assigns[:current_user] do
+        %User{condition: con} -> Condition.feedback?(con)
         _ -> true
       end
 
