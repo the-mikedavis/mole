dbd5ed900abc7d97bec3943c7be36176b4470f9a
Michael Davis <mcd164@miami.edu>
2018-07-30 21:26:27 -0500

    Only take the functions I want, load keys better

--- lib/mole_web/plugs/survey.ex
+++ lib/mole_web/plugs/survey.ex
@@ -9,21 +9,20 @@ defmodule MoleWeb.Plugs.Survey do
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    survey_id = get_session(conn, @key)
+    user = conn.assigns[:current_user]
+    survey_id = (user && user.survey_id) || get_session(conn, @key)
 
-    with %User{survey_id: nil} = user <- conn.assigns.current_user,
-         do: Accounts.update_user(user, %{survey_id: survey_id})
+    with %User{@key => nil} <- user,
+         do: Accounts.update_user(user, %{@key => survey_id})
 
     assign(conn, @key, survey_id)
   end
 
-  def load_surey(conn) do
+  def load_survey(conn) do
     survey_id = conn.assigns.survey_id
     survey = survey_id && Content.get_survey!(survey_id)
     assign(conn, :current_survey, survey)
   end
 
   def put_survey(conn, survey_id), do: put_session(conn, @key, survey_id)
-
-  def delete_survey(conn), do: delete_session(conn, @key)
 end
