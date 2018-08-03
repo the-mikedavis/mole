1cee39e4fd4b2219f247960f21bbc4b85e131b5e
Michael Davis <mcd164@miami.edu>
2018-08-02 20:58:35 -0500

    Make the index page usable (links)

--- test/mole_web/controllers/page_controller_test.exs
+++ test/mole_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MoleWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to Whack-a-Mole!"
   end
 end
