ef7c67fe25c28968959e011175df7dd283924e0a
Michael Davis <mcd164@miami.edu>
2018-08-04 15:48:00 -0500

    Unused alias

--- test/mole/gameplay_server_test.exs
+++ test/mole/gameplay_server_test.exs
@@ -1,6 +1,5 @@
 defmodule Mole.GameplayServerTest do
   use ExUnit.Case, async: true
-  alias Mole.GameplayServer, as: GS
 
   setup do
     on_exit(fn -> Agent.update(__MODULE__, fn _ -> %{} end) end)
