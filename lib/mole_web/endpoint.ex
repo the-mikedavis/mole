7c5108f8f09ad2eb6e6811e52e79e6ea1d6d883a
Michael Davis <mcd164@miami.edu>
2018-08-04 17:50:02 -0500

    Just use the port env from config

--- lib/mole_web/endpoint.ex
+++ lib/mole_web/endpoint.ex
@@ -50,11 +50,7 @@ defmodule MoleWeb.Endpoint do
 
   def init(_key, config) do
     if config[:load_from_system_env] do
-      port =
-        System.get_env("PORT") ||
-          raise "expected the PORT environment variable to be set"
-
-      {:ok, Keyword.put(config, :http, [:inet6, port: port])}
+      {:ok, Keyword.put(config, :http, [:inet6])}
     else
       {:ok, config}
     end
