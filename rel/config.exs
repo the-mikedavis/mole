fa0b9fe208bf3bae6c040af6d8e12a2093dc8c02
Michael Davis <mcd164@miami.edu>
2018-08-05 16:00:07 -0500

    Format the rel directory

--- rel/config.exs
+++ rel/config.exs
@@ -7,14 +7,13 @@ Path.join(["rel", "plugins", "*.exs"])
 |> Enum.map(&Code.eval_file(&1))
 
 use Mix.Releases.Config,
-    # This sets the default release built by `mix release`
-    default_release: :default,
-    # This sets the default environment used by `mix release`
-    default_environment: Mix.env()
+  # This sets the default release built by `mix release`
+  default_release: :default,
+  # This sets the default environment used by `mix release`
+  default_environment: Mix.env()
 
 # For a full list of config options for both releases
 # and environments, visit https://hexdocs.pm/distillery/configuration.html
-
 
 # You may define one or more environments in this file,
 # an environment's settings will override those of a release
@@ -28,15 +27,21 @@ environment :dev do
   # It is recommended that you build with MIX_ENV=prod and pass
   # the --env flag to Distillery explicitly if you want to use
   # dev mode.
-  set dev_mode: true
-  set include_erts: false
-  set cookie: :"oFVWQp%p%TO;{F]NtC[42rl)X~~)VNKUpf0&[*{aL(B`zT`mroOXs6*Bdgw]k*)/"
+  set(dev_mode: true)
+  set(include_erts: false)
+
+  set(
+    cookie: :"oFVWQp%p%TO;{F]NtC[42rl)X~~)VNKUpf0&[*{aL(B`zT`mroOXs6*Bdgw]k*)/"
+  )
 end
 
 environment :prod do
-  set include_erts: true
-  set include_src: false
-  set cookie: :"$F<l|D[wEk3j1qOnANqoqfiWAvJ0<w/2R$HU^UUz4N`B:.8s@:c3pUicmwS.t{K{"
+  set(include_erts: true)
+  set(include_src: false)
+
+  set(
+    cookie: :"$F<l|D[wEk3j1qOnANqoqfiWAvJ0<w/2R$HU^UUz4N`B:.8s@:c3pUicmwS.t{K{"
+  )
 end
 
 # You may define one or more releases in this file.
@@ -45,19 +50,24 @@ end
 # will be used by default
 
 release :mole do
-  {git_describe_version_raw, _} = System.cmd("git", ["describe", "--tags", "--long", "--always"])
+  {git_describe_version_raw, _} =
+    System.cmd("git", ["describe", "--tags", "--long", "--always"])
+
   git_describe_version = String.trim(git_describe_version_raw)
 
-  set version: git_describe_version
+  set(version: git_describe_version)
 
-  set applications: [
-    :runtime_tools,
-    :neotoma,
-    :phoenix_html
-  ]
+  set(
+    applications: [
+      :runtime_tools,
+      :neotoma,
+      :phoenix_html
+    ]
+  )
 
-  set commands: [
-    migrate: "rel/commands/migrate.sh"
-  ]
+  set(
+    commands: [
+      migrate: "rel/commands/migrate.sh"
+    ]
+  )
 end
-
