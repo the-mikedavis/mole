0f65910528086b6e09bebee65cf5a920339bed08
Michael Davis <mcd164@miami.edu>
2018-08-05 15:24:08 -0500

    Server true

--- config/prod.exs
+++ config/prod.exs
@@ -29,7 +29,8 @@ config :mole, MoleWeb.Endpoint,
   http: [port: "${MOLE_PORT}"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: "${MOLE_SECRET_KEYBASE}",
-  root: "."
+  root: ".",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
