491891d4f4df0eb2cf737a1716cad8f361297a2e
Michael Davis <mcd164@miami.edu>
2018-10-19 04:14:39 +0000

    Give feedback even when rerouting

--- assets/js/next_listener.js
+++ assets/js/next_listener.js
@@ -5,8 +5,7 @@ import constants from './constants'
 function listener(event) {
   if (event.detail.reroute)
     setTimeout(() => window.location.href = event.detail.path, constants.time_buffer)
-  else
-    image_listener(event)
+  image_listener(event)
 }
 
 export default listener
