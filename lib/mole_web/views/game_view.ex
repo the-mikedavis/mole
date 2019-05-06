52a34d560d9070ef5b69193e1048ebbd5a4ef17a
Michael Davis <mcarsondavis@gmail.com>
2019-05-06 16:41:13 -0400

    Improve leaderboards
    Closes #19

--- lib/mole_web/views/game_view.ex
+++ lib/mole_web/views/game_view.ex
@@ -4,4 +4,7 @@ defmodule MoleWeb.GameView do
   alias Mole.Content
 
   def image_path(image), do: Content.static_path(image)
+
+  def setorsets(1), do: "1 set"
+  def setorsets(n), do: "#{n} sets"
 end
